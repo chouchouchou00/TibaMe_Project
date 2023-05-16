75337aa8725ccdbf5cb8bee473fa1a91f51f2bbd
HuangRong <rock60809@gmail.com>
2023-05-17 02:30:40 +0800

    AddCommonQA/andConnect

---
+++ Shocker Project/Shocker Project/Controllers/CommonProblemController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Shocker_Project.Controllers
+{
+    public class CommonProblemController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
