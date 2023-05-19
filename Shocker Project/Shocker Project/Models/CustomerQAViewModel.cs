f20c73f8b7ce9bdcb05f16f0eb0af06744ab77f3
HuangRong <rock60809@gmail.com>
2023-05-20 00:55:45 +0800

    CompeletSeverQuestionToSSMS

---
+++ Shocker Project/Shocker Project/Models/CustomerQAViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Build.Framework;
+using Shocker_Project.Controllers;
+using System.ComponentModel.DataAnnotations;
+using RequiredAttribute = System.ComponentModel.DataAnnotations.RequiredAttribute;
+
+namespace Shocker_Project.Models
+{
+    public class CustomerQAViewModel
+    {
+   
+        public int CaseId { get; set; }
+        [Required(ErrorMessage = "請選擇類型")]
+        [Display(Name = "請選擇問題類別")]
+        public int QuestionCategoryId { get; set; }
+        public string? UserAccount { get; set; }
+        public string? AdminAccount { get; set; }
+        [Required(ErrorMessage = "請輸入您遇到的問題")]
+        [Display(Name = "請輸入問題")]
+        public string? Description { get; set; }
+        public string? Status { get; set; }
+        public DateTimeOffset? CloseDate { get; set; }
+        public string? Reply { get; set; }
+
+
+    }
+}
