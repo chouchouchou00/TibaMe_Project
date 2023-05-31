86b90016b45cf9d53f109b68cebc19a14d01f2de
HuangRong <rock60809@gmail.com>
2023-06-01 02:03:23 +0800

    CouponUESVue3debug

---
+++ Shocker Project/Shocker Project/Areas/Admin/Controllers/CouponController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Shocker_Project.Areas.Admin.Models;
+using Shocker_Project.Models;
+
+namespace Shocker_Project.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CouponController : Controller
+    {
+        private readonly db_a98a02_thm101team1001Context _context;
+        public CouponController(db_a98a02_thm101team1001Context context) 
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<JsonResult> DisaplayCoupon() 
+        {
+            var DC = _context.Coupons
+                .Select(x => new
+                {
+                    CouponId = x.CouponId,
+                    ExpirationDate = x.ExpirationDate,
+                    HolderAccount = x.HolderAccount,
+                    ProductCategoryId = x.ProductCategoryId,
+                    Discount = x.Discount,
+                    Status = x.Status,
+                    PublisherAccount = x.PublisherAccount,
+                });
+            return Json(DC);
+        }
+        [HttpPost]
+        public async Task<JsonResult> CreateCoupon([FromBody] CouponsViewModels cvm)
+        {
+            if (ModelState.IsValid)
+            {
+                //cvm.PublisherAccount = "Admin1";
+                Coupons coupons = new Coupons()
+                {
+                    ExpirationDate = cvm.ExpirationDate,
+                    HolderAccount = cvm.HolderAccount,
+                    ProductCategoryId = cvm.ProductCategoryId,
+                    Discount = cvm.Discount,
+                    Status = cvm.Status,
+                    PublisherAccount = cvm.PublisherAccount,
+                    
+                };
+
+                _context.Coupons.Add(coupons);
+                _context.SaveChanges();
+
+            }
+            return Json(new { Message = "回復完畢" });
+        }
+    }
+}
---
+++ Shocker Project/Shocker Project/Areas/Admin/Models/CouponsViewModels.cs
@@ -0,0 +1,24 @@
+﻿using Shocker_Project.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace Shocker_Project.Areas.Admin.Models
+{
+    public class CouponsViewModels
+    {
+        //public Coupons()
+        //{
+        //    OrderDetails = new HashSet<OrderDetails>();
+        //}
+
+        public string CouponId { get; set; }
+        public DateTime ExpirationDate { get; set; }
+        public string HolderAccount { get; set; }
+        public int ProductCategoryId { get; set; }
+        //[Required(ErrorMessage="請輸入折扣小於1")]
+        public decimal Discount { get; set; }
+        public string Status { get; set; }
+        public string PublisherAccount { get; set; }
+
+        
+    }
+}
