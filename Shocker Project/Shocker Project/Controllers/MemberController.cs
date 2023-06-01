62f21f98bac3e4157675c12e8b6a85d023848c3e
CHOU <chouchouchou@kimo.com>
2023-06-01 17:50:05 +0800

    no message

--- Shocker Project/Shocker Project/Controllers/MemberController.cs
+++ Shocker Project/Shocker Project/Controllers/MemberController.cs
@@ -1,9 +1,25 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration.UserSecrets;
+//using System.Web.Security;
+using Shocker_Project.Models;
+using System.Web.Providers.Entities;
 
 namespace Shocker_Project.Controllers
 {
+    
     public class MemberController : Controller
     {
+        private readonly db_a98a02_thm101team1001Context _context;
+        private readonly IWebHostEnvironment _environment;
+        private readonly IConfiguration _configuration;
+
+        public MemberController(db_a98a02_thm101team1001Context context, IWebHostEnvironment environment, IConfiguration configuration)
+        {
+            _context = context;
+            _environment = environment;
+            _configuration = configuration;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -14,7 +30,30 @@ namespace Shocker_Project.Controllers
             return View();
         }
 
-        public IActionResult SignUp()
+        [HttpPost]
+        public IActionResult SignIn(string Account, string Password)
+        {
+            var member = _context.Users
+                .Where(m => m.Account == Account && m.Password == Password)
+                .FirstOrDefault();
+
+            if (member == null)
+            {
+                ViewBag.Message = "帳密有錯，登入失敗";
+                return View();
+            }
+
+            //Session["Welcome"] = member.Name + "歡迎光臨";
+            //FormAuthentication.RedirectFromLoginPage(Account, true);
+            return Redirect("~/Home");
+        }
+
+        public IActionResult SignOut()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
         {
             return View();
         }
--- Shocker Project/Shocker Project/Program.cs
+++ Shocker Project/Shocker Project/Program.cs
@@ -12,6 +12,7 @@ namespace Shocker_Project
 		{
 			var builder = WebApplication.CreateBuilder(args);
 
+			builder.Services.AddControllersWithViews();
 			// Add services to the DI container.
 			var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 			builder.Services.AddDbContext<ApplicationDbContext>(options =>
@@ -60,7 +61,7 @@ namespace Shocker_Project
 			app.MapControllerRoute(
 				name: "default",
 				pattern: "{controller=Home}/{action=Index}/{id?}");
-			app.MapRazorPages();
+			//app.MapRazorPages();
 
 			app.Run();
 		}
